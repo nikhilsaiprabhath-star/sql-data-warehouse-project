0173b53bcb666cc9cf91de5da9ae15e35d30db0e
nikhilsaiprabhath-star <nikhilsaiprabhath@gmail.com>
2026-09-14 13:44:49 +0200

    Create 'Datawarehouse' database and schemas
    This script creates a new database named 'Datawarehouse' and sets up three schemas: 'silver', 'bronze', and 'gold'.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,27 @@
+/* 
+=================================================================================
+
+Create Database and Schemas 
+
+Script purpose: 
+              This script creates a new database named 'Datawarehouse'.
+              Additionally, the script sets up three schemas within the database: 'silver', 'bronze', 'gold'
+
+*/
+
+
+
+--Create Database 'Datawarehouse'
+
+Create Database Datawarehouse;
+
+--Create Schemas
+
+Create schema bronze;
+
+create Schema silver;
+
+Create schema gold;
+
+
 
